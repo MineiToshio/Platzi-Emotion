53e52d430dbc00075823b7d8725aec98f4e7b326
Toshio Minei <sminei10@gmail.com>
2018-05-10 03:49:12 -0500

    first upload of the proyect

---
+++ src/EmotionPlatzi.Web/Controllers/EmoUploaderController.cs
@@ -0,0 +1,72 @@
+﻿using EmotionPlatzi.Web.Models;
+using EmotionPlatzi.Web.Util;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmotionPlatzi.Web.Controllers
+{
+    public class EmoUploaderController : Controller
+    {
+        string serverFolderPath;
+        EmotionHelper emoHelper;
+        string key;
+        EmotionPlatziWebContext db = new EmotionPlatziWebContext();
+
+        public EmoUploaderController()
+        {
+            serverFolderPath = ConfigurationManager.AppSettings["UPLOAD_DIR"];
+            key = ConfigurationManager.AppSettings["EMOTION_KEY"];
+            emoHelper = new EmotionHelper(key);
+        }
+
+
+        // GET: EmoUploader
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(HttpPostedFileBase file)
+        {
+            try
+            {
+                //file != null && file.ContentLength > 0
+                if (file?.ContentLength > 0)
+                {
+                    var pictureName = Guid.NewGuid().ToString();
+                    pictureName += Path.GetExtension(file.FileName);
+
+                    var route = Server.MapPath(serverFolderPath);
+
+                    route = route + "/" + pictureName;
+
+                    file.SaveAs(route);
+
+                    var emoPicture = await emoHelper.DetectAndExtractFacesAsync(file.InputStream);
+                    //var emoPicture = new EmoPicture();
+
+                    emoPicture.Name = file.FileName;
+                    emoPicture.Path = $"{serverFolderPath}/{pictureName}";
+
+                    db.EmoPictures.Add(emoPicture);
+                    await db.SaveChangesAsync();
+
+                    return RedirectToAction("Details", "EmoPictures", new { Id = emoPicture.Id });
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ src/EmotionPlatzi.Web/Util/EmotionHelper.cs
@@ -0,0 +1,86 @@
+﻿using EmotionPlatzi.Web.Models;
+using Microsoft.ProjectOxford.Common.Contract;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EmotionPlatzi.Web.Util
+{
+    public class EmotionHelper
+    {
+        public FaceServiceClient emoClient;
+
+        public EmotionHelper(string key)
+        {
+            emoClient = new FaceServiceClient(key, "https://westcentralus.api.cognitive.microsoft.com/face/v1.0");
+
+        }
+
+        public async Task<EmoPicture> DetectAndExtractFacesAsync(Stream imageStream)
+        {
+            IEnumerable<FaceAttributeType> faceAttributes = new FaceAttributeType[] { FaceAttributeType.Emotion };
+            Face[] faces = await emoClient.DetectAsync(imageStream, false, false, faceAttributes);
+
+            var emoPicture = new EmoPicture();
+
+            emoPicture.Faces = ExtractFaces(faces, emoPicture);
+
+            return emoPicture;
+        }
+
+        private ObservableCollection<EmoFace> ExtractFaces(Face[] faces, EmoPicture emoPicture)
+        {
+            var listaFaces = new ObservableCollection<EmoFace>();
+
+            foreach (var face in faces)
+            {
+                var emoFace = new EmoFace()
+                {
+                    X = face.FaceRectangle.Left,
+                    Y = face.FaceRectangle.Top,
+                    Width = face.FaceRectangle.Width,
+                    Height = face.FaceRectangle.Height,
+                    Picture = emoPicture
+                };
+                
+                emoFace.Emotions = ProcessEmotions(face.FaceAttributes.Emotion, emoFace);
+                listaFaces.Add(emoFace);
+            }
+
+            return listaFaces;
+        }
+
+        private ObservableCollection<EmoEmotion> ProcessEmotions(EmotionScores scores, EmoFace emoFace)
+        {
+            var emotionList = new ObservableCollection<EmoEmotion>();
+            var properties = scores.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var filterProperties = properties.Where(p => p.PropertyType == typeof(float));
+
+
+            var emoType = EmoEmotionEnum.Undetermined;
+            foreach (var prop in filterProperties)
+            {
+                if (!Enum.TryParse<EmoEmotionEnum>(prop.Name, out emoType))
+                    emoType = EmoEmotionEnum.Undetermined;
+
+                var emoEmotion = new EmoEmotion();
+                emoEmotion.Score = (float)prop.GetValue(scores);
+                emoEmotion.EmotionType = emoType;
+                emoEmotion.EmoFaceId = emoFace.Id;
+                emoEmotion.Face = emoFace;
+
+                emotionList.Add(emoEmotion);
+            }
+
+            return emotionList;
+        }
+    }
+}
